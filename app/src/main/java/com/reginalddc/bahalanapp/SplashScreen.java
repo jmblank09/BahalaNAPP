db88aa92b0e084ea97471f06066dfcc3da108af7
Reginald Dela Cruz <regiinalddelacruz@gmail.com>
2017-02-27 15:49:54 +0800

    First Commit

---
+++ app/src/main/java/com/reginalddc/bahalanapp/SplashScreen.java
@@ -0,0 +1,39 @@
+package com.reginalddc.bahalanapp;
+
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        TextView appName = (TextView) findViewById(R.id.appname_textView);
+        Typeface typeface = Typeface.createFromAsset(getAssets(), "fonts/arial_rounded.ttf");
+        appName.setTypeface(typeface);
+
+        Thread loadingThread = new Thread() {
+
+            @Override
+            public void run() {
+                try {
+                    super.run();
+                    sleep(2000);
+                } catch (Exception e) {
+
+                } finally {
+                    Intent intent = new Intent(SplashScreen.this,
+                            MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        };
+        loadingThread.start();
+    }
+}
